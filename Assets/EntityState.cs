90f4ded4d374744d6d30f348ba0e4b329d10dac0
Arif Sahin <marfsahin@gmail.com>
2025-07-25 05:54:23 +0300

    initial commit

---
+++ Assets/EntityState.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public abstract class EntityState
+{
+    protected Player player;
+    protected StateMachine stateMachine;
+    protected string stateName;
+
+    public EntityState(Player player, StateMachine stateMachine, string stateName)
+    {
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.stateName = stateName; 
+    }
+
+    public virtual void Enter()
+    {
+        //everytime state will be changed, enter will be called
+        Debug.Log("I enter " + stateName);
+    }
+
+    public virtual void Update()
+    {
+        // we going to run logic of the state here
+        Debug.Log("I run update of " + stateName);
+    }
+
+    public virtual void Exit()
+    {
+        //this will be called, everytime we exit state and changed to a new one
+        Debug.Log("I exit " + stateName);
+    }
+    
+}
