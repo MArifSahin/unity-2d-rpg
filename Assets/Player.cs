f4be6d8b67025a6f8df752367760ae784fee3eeb
Arif Sahin <marfsahin@gmail.com>
2025-07-25 06:11:21 +0300

    feat: new input system for player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private PlayerInputSet input;
     public StateMachine stateMachine;
+
+
     public Player_IdleState idleState { get; private set; }
     public Player_MoveState moveState { get; private set; }
 
     private void Awake()
     {
         stateMachine = new StateMachine();
+        input = new PlayerInputSet();
+
         idleState = new Player_IdleState(this, stateMachine, "idle");
         moveState = new Player_MoveState(this, stateMachine, "move");
+    }
+
+    private void OnEnable() {
+        input.Enable();
+    }
+
+    private void OnDisable() {
+        input.Disable();
     }
 
     private void Start()
