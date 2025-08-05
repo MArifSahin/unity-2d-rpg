ffb3ea2a0d3951eb0524678f0f7dbb92d3bb02a6
Arif Sahin <marfsahin@gmail.com>
2025-08-05 12:12:08 +0300

    feat: counter attack state & logic

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : Entity
     public Enemy_AttackState attackState;
     public Enemy_BattleState battleState;
     public Enemy_DeadState deadState;
+    public Enemy_StunnedState stunnedState;
 
     [Header("Battle Details")]
     public float battleMoveSpeed = 3;
@@ -15,6 +16,11 @@ public class Enemy : Entity
     public float battleTimeDuration = 5;
     public float minRetreatDistance = 1;
     public Vector2 retreatVelocity;
+
+    [Header("Stunned Details")]
+    public float stunnedDuration = 2;
+    public Vector2 stunnedVelocity = new Vector2(7, 7);
+    protected bool canBeStunned;
 
     [Header("Movement Details")]
     public float idleTime = 2;
@@ -27,6 +33,8 @@ public class Enemy : Entity
     [SerializeField] private Transform playerCheck;
     [SerializeField] private float playerCheckDistance = 10;
     public Transform player { get; private set; }
+
+    public void EnableCounterWindow(bool enable) => canBeStunned = enable;
 
     public override void EntityDeath()
     {
